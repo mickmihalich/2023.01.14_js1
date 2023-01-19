84b2c3b9f603401d7f634dcf283cef51a78edabd
mickmihalich <mickmihalich@gmail.com>
2023-01-19 12:41:05 +0200

    додам з хабра деякий код для перехвату Ctrl+S (No)

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const writer = document.querySelector('.writer');
 
 //Змінна зі збереженим текстом
-var myNotes;
+let myNotes;
 //сподобався тернарний оператор =^_^=
 //пусте за замовченням
 (localStorage.getItem('myNotes')) ? myNotes = localStorage.getItem('myNotes') : myNotes = "";
@@ -29,9 +29,8 @@ writer.addEventListener('input', () => {
     
 });
 
-const saver = document.querySelector(".save");
-saver.addEventListener('mouseenter', () => {
-    var temp = writer.value;
+function savering() {
+    let temp = writer.value;
     if (temp != myNotes) {
         console.log('myNotes save: ', temp);
         myNotes = temp;
@@ -47,14 +46,18 @@ saver.addEventListener('mouseenter', () => {
         saver.innerText = "Збережено";  
     } else {
         console.log('нічого не змінилося');
-    }    
-      
+    }     
+}
+
+const saver = document.querySelector(".save");
+saver.addEventListener('mouseenter', () => {
+    savering();
 });
 
 const btnReset = document.querySelector(".reset");
 btnReset.addEventListener('click', () => {
     writer.value = "";
-    var temp = writer.value;
+    let temp = writer.value;
     if (temp != myNotes) {
         console.log('myNotes стерто!');
         myNotes = temp;
@@ -63,3 +66,41 @@ btnReset.addEventListener('click', () => {
         console.log('вже стерто!');
     }
 });
+
+//додам з хабра деякий код для перехвату Ctrl+S (Але він поки що не відпрацьовує)
+//  © https://habr.com/ru/post/25511/
+// Функция для добавления обработчиков событий
+function addHandler(object, event, handler, useCapture) {
+    if (object.addEventListener) {
+        object.addEventListener(event, handler, useCapture ? useCapture : false);
+    } else if (object.attachEvent) {
+        object.attachEvent('on' + event, handler);
+    } else alert("Add handler is not supported");
+}
+    
+// Определяем браузеры
+var ua = navigator.userAgent.toLowerCase();
+var isIE = (ua.indexOf("msie") != -1 && ua.indexOf("opera") == -1);
+var isGecko = (ua.indexOf("gecko") != -1);
+
+// Добавляем обработчики
+if (isIE) addHandler (document, "keydown", hotSave);
+else addHandler (document, "keypress", hotSave);
+
+function hotSave(evt) {
+    // Получаем объект event
+    evt = evt || window.event;
+    var key = evt.keyCode || evt.which;
+    // Определяем нажатие Ctrl+S
+    key = String.fromCharCode(key).toLowerCase() == "s";
+    if (evt.ctrlKey && key) {
+        // Блокируем появление диалога о сохранении
+        if(evt.preventDefault) evt.preventDefault();
+        evt.returnValue = false;
+        // Запускаем любую функцию, по желанию
+        savering();
+        // Возвращаем фокус в окно
+        window.focus();
+        return false;
+    }
+}
